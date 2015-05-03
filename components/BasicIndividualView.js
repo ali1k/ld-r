3f2f5ec2faaba37a74f11fc45aa908e79222e230
ali1k <hyperir@gmail.com>
2015-05-03 17:51:15 +0200

    activate URIs in basicView

--- components/BasicIndividualView.js
+++ components/BasicIndividualView.js
@@ -2,9 +2,15 @@ import React from 'react';
 
 class BasicIndividualView extends React.Component {
     render() {
+        let outputDIV;
+        if(this.props.spec.valueType === 'uri'){
+            outputDIV = <a href={this.props.spec.value} target="_blank"> {this.props.spec.value} </a>;
+        }else{
+            outputDIV = <span> {this.props.spec.value} </span>;
+        }
         return (
             <div className="ui">
-                {this.props.spec.value}
+                {outputDIV}
             </div>
         );
     }
