486d4386f95aaa17d9c593dbee0e474432e6e5f8
ali1k <hyperir@gmail.com>
2015-05-23 23:28:20 +0200

    edited about ld-reactor

--- components/About.js
+++ components/About.js
@@ -9,7 +9,39 @@ class About extends React.Component {
                     <div className="ui segment content">
                         <h2 className="ui header">About</h2>
                         <p>
-                            LD-Reactor is an attempt to create reactive & reusable Linked Data components!
+                            Documentation is available on <a href="https://github.com/ali1k/ld-reactor">Github</a>. <br/>LD-Reactor is developed by:
+                            <div className="blue ui card item">
+                              <div className="content">
+                                <div className="header"><a href="http://vu.nl" target="_blank"><img className="ui centered medium image" src="/assets/img/VU_logo.png" /></a></div>
+                                <div className="meta"><a href="http://www.networkinstitute.org/" target="_blank">Department of Computer Science  & <br/> The Network Institute</a></div>
+                                <div className="description">
+                                    VU University Amsterdam <br/>
+                                 de Boelelaan 1081a<br/> 1081HV Amsterdam<br/> The Netherlands <br/>
+                                 </div>
+                              </div>
+                          </div>
+                          <div className="ui yellow card item">
+                            <div className="content">
+                                <div className="extra content">
+                                  <h4 className="ui header"> Contacts: </h4>
+                                    <div className="ui list">
+                                      <div className="item">
+                                          tel (+31)-20-598 7731/7718
+                                      </div>
+                                      <div className="item">
+                                        <div className="ui label">
+                                          <a href="mailto:frank.van.harmelen@vu.nl"><i className="mail icon"></i> Prof. &nbsp; Frank van Harmelen &nbsp; &nbsp; &nbsp; &nbsp;</a>
+                                        </div>
+                                      </div>
+                                      <div className="item">
+                                        <div className="ui label">
+                                          <a href="mailto:a.khalili@vu.nl@vu.nl"><i className="mail icon"></i> Dr. &nbsp; Ali Khalili &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;</a>
+                                        </div>
+                                      </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
                         </p>
                     </div>
                 </div>
