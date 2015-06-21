1458ec0ca6933b4a58491a94c1860676267870dc
ali1k <hyperir@gmail.com>
2015-06-21 12:01:43 +0200

    minor

--- configs/reactor.config.js
+++ configs/reactor.config.js
@@ -14,7 +14,7 @@ export default {
     //will prevent access if not logged in
     enableAuthentication: 0,
     //graph that stores users data, must be loaded beforehand
-    authGraphName: ['https://github.com/ali1k/ld-reactor/blob/master/plugins/authentication/schema/users.ttl#'],
+    authGraphName: ['https://ld-r.org/users'],
     //used when creating random resources
     dynamicResourceDomain: ['http://example.org'],
     //will allow super users to confirm and activate regiastered users
@@ -39,11 +39,75 @@ export default {
         },
         //for each graph name, you can define custom configs.
         //if no custom config is defined for a specific graph, the generic config will be used.
-        'https://github.com/ali1k/ld-reactor/blob/master/plugins/authentication/schema/users.ttl#': {
+        'https://ld-r.org/users': {
             readOnly: 0,
             useCategories: 0,
             config: {
-
+                'http://www.w3.org/1999/02/22-rdf-syntax-ns#type': {
+                    isHidden: 1
+                },
+                'http://xmlns.com/foaf/0.1/accountName': {
+                    label: ['Username'],
+                    readOnly: 1
+                },
+                'http://xmlns.com/foaf/0.1/firstName': {
+                    label: ['First Name']
+                },
+                'http://xmlns.com/foaf/0.1/lastName': {
+                    label: ['Last Name']
+                },
+                'http://xmlns.com/foaf/0.1/mbox': {
+                    label: ['Email Address'],
+                    readOnly: 1
+                },
+                'https://github.com/ali1k/ld-reactor/blob/master/vocabulary/index.ttl#password': {
+                    label: ['Password'],
+                    viewer: ['PasswordView'],
+                    editor: ['PasswordInput']
+                },
+                'https://github.com/ali1k/ld-reactor/blob/master/vocabulary/index.ttl#editorOfGraph': {
+                    label: ['Editor of Graph'],
+                    allowNewValue: 1
+                },
+                'https://github.com/ali1k/ld-reactor/blob/master/vocabulary/index.ttl#editorOfResource': {
+                    label: ['Editor of Resource'],
+                    allowNewValue: 1
+                },
+                'https://github.com/ali1k/ld-reactor/blob/master/vocabulary/index.ttl#editorOfProperty': {
+                    label: ['Editor of Property'],
+                    allowNewValue: 1,
+                    allowExtension: 1,
+                    extensions: [
+                        {
+                            spec: {
+                                propertyURI: 'https://github.com/ali1k/ld-reactor/blob/master/vocabulary/index.ttl#resource',
+                                value: 'http://exampleResource.org',
+                                valueType: 'uri'
+                            },
+                            config: {
+                                hint: ['Resource URI under which the property is exposed.'],
+                                label: ['Resource']
+                            }
+                        },
+                        {
+                            spec: {
+                                propertyURI: 'https://github.com/ali1k/ld-reactor/blob/master/vocabulary/index.ttl#property',
+                                value: 'http://exampleProperty.org',
+                                valueType: 'uri'
+                            },
+                            config: {
+                                hint: ['Property URI'],
+                                label: ['Property']
+                            }
+                        }
+                    ]
+                },
+                'http://xmlns.com/foaf/0.1/organization': {
+                    label: ['Organization'],
+                    allowNewValue: 1,
+                    viewer: ['BasicDBpediaView'],
+                    editor: ['DBpediaInput']
+                }
             }
         }
     },
