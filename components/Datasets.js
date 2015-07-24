0cff254a475e29c7db3572ecd08ad28d20d40792
ali1k <hyperir@gmail.com>
2015-07-24 13:04:00 +0200

    fixed dataset lisast

--- components/Datasets.js
+++ components/Datasets.js
@@ -1,41 +1,30 @@
 import React from 'react';
-import {defaultGraphName, authGraphName, propertiesConfig, facetsConfig} from '../configs/reactor';
+import {defaultGraphName, authGraphName, config} from '../configs/reactor';
 
 class Datasets extends React.Component {
     componentDidMount() {
 
     }
     render() {
-        let dss = [], dfl, brws, output = [];
-        for(let graph in propertiesConfig){
-            if(graph !== authGraphName[0] && graph !== 'generic'){
-                brws = '';
-                dfl = '';
-                if(facetsConfig[graph]){
-                    brws = <a className="ui label" href={'/browse/' + encodeURIComponent(graph)} title="browse"><i className="zoom icon"></i>browse</a>;
-                }
-                if(graph === defaultGraphName[0]){
-                    dfl = <i className="ui green flag icon" title="default dataset"></i>;
-                }
-                dss.push(graph);
-                output.push(<div className="ui item" key={graph}> <div className="content"> <i className="ui blue icon cubes"></i> <a href={'/dataset/1/' + encodeURIComponent(graph)} title="go to resource list">{graph}</a> {brws} {dfl}</div> </div>);
-            }
-        }
-        //if only facet is set
-        for(let graph in facetsConfig){
-            if(graph !== authGraphName[0] && graph !== 'generic'){
-                if(dss.indexOf(graph) === -1){
-                    brws = '';
+        let sources = ['dataset', 'dataset_resource', 'dataset_property', 'dataset_object', 'dataset_resource_property', 'dataset_resource_object', 'dataset_property_object', 'dataset_resource_property_object'];
+        let dss = [], dfl, output = [];
+        sources.forEach(function(s){
+            for(let graph in config[s]){
+                if(graph !== authGraphName[0] && graph !== 'generic'){
                     dfl = '';
                     if(graph === defaultGraphName[0]){
                         dfl = <i className="ui green flag icon" title="default dataset"></i>;
                     }
-                    brws = <a className="ui label" href={'/browse/' + encodeURIComponent(graph)} title="browse"><i className="zoom icon"></i>browse</a>;
-                    dss.push(graph);
-                    output.push(<div className="ui item" key={graph}> <div className="content"> <i className="ui blue icon cubes"></i> <a href={'/dataset/1/' + encodeURIComponent(graph)} title="go to resource list">{graph}</a> {brws} {dfl}</div> </div>);
+                    if(dss.indexOf(graph) === -1){
+                        dss.push(graph);
+                        output.push(<div className="ui item" key={graph}> <div className="content"> <i className="ui blue icon cubes"></i> <a href={'/dataset/1/' + encodeURIComponent(graph)} title="go to resource list">{graph}</a> {dfl}</div> </div>);
+                    }
                 }
             }
-        }
+        });
+        let info = <div className="ui blue message">
+                        The list contains only the datasets for which at least one <b>config scope</b> is found!
+                   </div>;
         if(!dss.length){
             if(defaultGraphName[0]){
                 output.push(<div className="ui item" key={defaultGraphName[0]}> <div className="content"> <i className="ui blue icon cubes"></i> <a href={'/dataset/1/' + encodeURIComponent(defaultGraphName[0])} title="go to resource list">{defaultGraphName[0]}</a> </div> </div>);
@@ -47,6 +36,7 @@ class Datasets extends React.Component {
         return (
             <div className="ui page grid" ref="datasets">
                 <div className="ui column">
+                    {dss.length ? <div>{info}</div> : ''}
                     <div className="ui segment">
                         <h1>Datasets</h1>
                         <div className="ui big divided list">
