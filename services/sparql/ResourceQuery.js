7d9e6c430f63bf5138481daeb251964f344cbdec
Antonis Loizou <a.loizou@vu.nl>
2015-07-29 14:12:38 +0200

    Addint let in front of uninitialised vars

--- services/sparql/ResourceQuery.js
+++ services/sparql/ResourceQuery.js
@@ -87,9 +87,9 @@ class ResourceQuery{
             ex ='';
         }
         if(oldObjectValue){
-            tmp = getQueryDataTypeValue(valueType, dataType, oldObjectValue);
-            newValue = tmp.value;
-            dtype = tmp.dtype;
+            let tmp = getQueryDataTypeValue(valueType, dataType, oldObjectValue);
+            let newValue = tmp.value;
+            let dtype = tmp.dtype;
           //if we just want to delete a specific value for multi-valued ones
           this.query = 'DELETE ' + ex + ' {<'+ resourceURI +'> <'+ propertyURI +'> ?v} ' + this.addTriple(graphName, resourceURI, propertyURI, newObjectValue, valueType, dataType) + ' WHERE { <'+ resourceURI +'> <'+ propertyURI +'> ?v . FILTER(' + dtype + '(?v)= '+ oldObjectValue +' ) }';
         }else{
