cba9f25e6dfa0bf5a64630c5044301be85d82b90
ali1k <hyperir@gmail.com>
2015-08-01 20:44:22 +0200

    added scroll to top

--- components/resource/Resource.js
+++ components/resource/Resource.js
@@ -6,6 +6,12 @@ class Resource extends React.Component {
     constructor(props) {
         super(props);
     }
+    componentDidMount() {
+        //scroll to top of the page
+        let body = $("html, body");
+        body.stop().animate({scrollTop:0}, '500', 'swing', function() {
+        });
+    }
     includesProperty(list, resource, property) {
         let out = false;
         list.forEach(function(el) {
