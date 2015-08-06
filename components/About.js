ef9c86c7b7168eabbd20e3c8d96f6499f489f60d
Ali Khalili <hyperir@gmail.com>
2015-08-06 13:49:39 +0200

    Update About.js

--- components/About.js
+++ components/About.js
@@ -35,7 +35,7 @@ class About extends React.Component {
                                         <a className="ui label animated zoomIn" href="mailto:a.khalili@vu.nl@vu.nl"><i className="mail icon"></i> Dr. &nbsp; Ali Khalili &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;</a>
                                       </div>
                                       <div className="item">
-                                        <a className="ui label animated zoomIn" href="mailto:a.loizou@vu.nl"><i className="mail icon"></i> Dr. &nbsp; Antoins Loizou &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;</a>
+                                        <a className="ui label animated zoomIn" href="mailto:a.loizou@vu.nl"><i className="mail icon"></i> Dr. &nbsp; Antonis Loizou &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;</a>
                                       </div>
                                     </div>
                                 </div>
