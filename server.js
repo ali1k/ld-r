048a67b462efa614addb76f6353d6a5be0c38da9
ali1k <hyperir@gmail.com>
2016-08-08 10:52:18 +0200

    upgraded npm packages

--- server.js
+++ server.js
@@ -110,7 +110,9 @@ server.use((req, res, next) => {
         debug('Rendering Application component into html');
         const markup = ReactDOM.renderToString(createElementWithContext(context));
         const htmlElement = React.createElement(HtmlComponent, {
-            clientFile: env === 'production' ? 'main.min.js' : 'main.js',
+            //clientFile: env === 'production' ? 'main.min.js' : 'main.js',
+            //use main.js for both dev and prod modes
+            clientFile: 'main.js',
             context: context.getComponentContext(),
             state: exposed,
             markup: markup
