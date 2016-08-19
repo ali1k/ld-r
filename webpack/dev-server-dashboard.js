489bf6d4ba158758a6d5e4645d18cad290af5f6f
ali1k <hyperir@gmail.com>
2016-08-19 16:22:21 +0200

    added webpack dashboard

---
+++ webpack/dev-server-dashboard.js
@@ -0,0 +1,33 @@
+let WebpackDevServer = require ('webpack-dev-server');
+let webpack = require ('webpack');
+let config = require ('./dev.config');
+let shell = require ('shelljs');
+let DashboardPlugin = require('webpack-dashboard/plugin');
+
+const host = process.env.HOST || '0.0.0.0';
+const mainPort = (process.env.PORT) || 3000;
+const devPort = (process.env.PORT + 1) || 3001;
+
+const options = {
+    //contentBase: `http://${host}:${port}`,
+    hot: true,
+    historyApiFallback: true,
+    //inline: true,
+    //lazy: false,
+    publicPath: config.output.publicPath,
+    proxy: {
+        '*': { target: `http://${host}:${devPort}` }
+    }
+    //stats: {
+        //colors: true
+    //}
+};
+
+const compiler = webpack(config);
+compiler.apply(new DashboardPlugin());
+
+new WebpackDevServer(compiler, options).listen(mainPort, host,  () => {
+    shell.env.PORT = shell.env.PORT || devPort;
+    shell.exec('"./node_modules/.bin/nodemon" start.js -e js,jsx',  () => {});
+    console.log('------>Webpack development server listening on %s:%s<--------', host, mainPort);
+});
