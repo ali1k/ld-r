827189fad3cab0ff082eafd2db285ffd6cbd2c8e
ali1k <hyperir@gmail.com>
2016-10-04 21:24:18 +0200

    improved image component

--- components/object/viewer/individual/BasicImageView.js
+++ components/object/viewer/individual/BasicImageView.js
@@ -2,9 +2,18 @@ import React from 'react';
 class BasicImageView extends React.Component {
     render() {
         let val, outputDIV;
+        let styleCl = {maxWidth: '400px'};
         val = this.props.spec.value;
+        if(this.props.config){
+            if(this.props.config.imageHeight){
+                styleCl['height'] = this.props.config.imageHeight;
+            }
+            if(this.props.config.imageWidth){
+                styleCl['width'] = this.props.config.imageWidth;
+            }
+        }
         if(this.props.spec.valueType === 'uri'){
-            outputDIV = <a href={val} target="_blank"> <img className="ui centered rounded image" style={{maxWidth: '400'}} src={val} alt={val} /> </a>;
+            outputDIV = <a href={val} target="_blank"> <img className="ui centered rounded image" style={styleCl} src={val} alt={val} /> </a>;
         }else{
             outputDIV = <span> {val} </span>;
         }
