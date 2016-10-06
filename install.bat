0abd33a12cb36a52ab8acd2d3ebcde5f78434cb1
ali1k <hyperir@gmail.com>
2016-10-06 14:12:36 +0200

    added a bat file for windows users

---
+++ install.bat
@@ -0,0 +1,21 @@
+echo "Installing configuration files..."
+if not exist "configs/general.js" (
+    copy configs/general.config.js configs/general.js
+)
+if not exist "configs/server.js" (
+    copy configs/server.config.js configs/server.js
+)
+if not exist "configs/reactor.js" (
+    copy configs/reactor.config.js configs/reactor.js
+)
+if not exist "configs/facets.js" (
+    copy configs/facets.config.js configs/facets.js
+)
+if not exist "plugins/email/config.js" (
+    copy plugins/email/config.sample.js plugins/email/config.js
+)
+echo "Installing bower packages..."
+bower install
+
+echo "Installing npm packages..."
+npm install
