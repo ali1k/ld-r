f46dc35963854fd25f72f59d00f23e0c4bc775ef
ali1k <hyperir@gmail.com>
2016-11-03 14:39:05 +0100

    more refactoring

--- components/reactors/ObjectReactor.js
+++ components/reactors/ObjectReactor.js
@@ -21,7 +21,7 @@ class ObjectReactor extends React.Component {
     includesProperty(list, resource, property) {
         let out = false;
         list.forEach(function(el) {
-            if (el.r === resource && el.p === property){
+            if (el.r === resource && el.p === property) {
                 out = true;
                 return out;
             }
@@ -29,37 +29,37 @@ class ObjectReactor extends React.Component {
         return out;
     }
     checkAccess(user, graph, resource, property) {
-        if(this.props.enableAuthentication) {
-            if(user){
-                if(parseInt(user.isSuperUser)){
+        if (this.props.enableAuthentication) {
+            if (user) {
+                if (parseInt(user.isSuperUser)) {
                     return {access: true, type: 'full'};
-                }else{
-                    if(graph && user.editorOfGraph.indexOf(graph) !== -1){
+                } else {
+                    if (graph && user.editorOfGraph.indexOf(graph) !== -1) {
                         return {access: true, type: 'full'};
-                    }else{
-                        if(resource && user.editorOfResource.indexOf(resource) !== -1){
+                    } else {
+                        if (resource && user.editorOfResource.indexOf(resource) !== -1) {
                             return {access: true, type: 'full'};
-                        }else{
-                            if(property && this.includesProperty(user.editorOfProperty, resource, property)){
+                        } else {
+                            if (property && this.includesProperty(user.editorOfProperty, resource, property)) {
                                 return {access: true, type: 'partial'};
-                            }else{
+                            } else {
                                 return {access: false};
                             }
                         }
                     }
                 }
-            }else{
+            } else {
                 return {access: false};
             }
-        }else{
+        } else {
             return {access: true, type: 'full'};
         }
     }
     //considers 0 elements
-    calculateValueCount (instances){
+    calculateValueCount(instances) {
         let count = 0;
         instances.forEach(function(v, i) {
-            if(instances[i]){
+            if (instances[i]) {
                 count++;
             }
         });
@@ -68,71 +68,75 @@ class ObjectReactor extends React.Component {
     //removes properties from an object
     configMinus(config, props) {
         let o = {};
-        for(let p in config) {
-            if(props.indexOf(p) === -1){
-                o [p] = config [p];
+        for (let p in config) {
+            if (props.indexOf(p) === -1) {
+                o[p] = config[p];
             }
         }
         return o;
     }
     render() {
         let self = this;
-        let list, user = this.context.getUser();
-        if(this.props.isNewValue){
-            list = <IndividualObject key="newValueInput" isNewValue={true} inEditMode={true} spec={self.props.spec} datasetURI={self.props.datasetURI} resource={self.props.resource} property={self.props.property}
-            onCreate={self.props.onCreateIndividualObject.bind(self)} config={self.configMinus(self.props.config, ['objectReactor'])} />;
-        }else{
+        let list,
+            user = this.context.getUser();
+        if (this.props.isNewValue) {
+            list = <IndividualObject key="newValueInput" isNewValue={true} inEditMode={true} spec={self.props.spec} datasetURI={self.props.datasetURI} resource={self.props.resource} property={self.props.property} onCreate={self.props.onCreateIndividualObject.bind(self)} config={self.configMinus(self.props.config, ['objectReactor'])}/>;
+        } else {
             //check if it is the only value of a property -> used to hide delete button
             let isOnlyChild = (this.calculateValueCount(this.props.spec.instances) === 1);
-            let objectReactorType, accessLevel, readOnly;
-            if(this.props.config){
-                if(!this.props.config.objectReactor){
+            let objectReactorType,
+                accessLevel,
+                readOnly;
+            if (this.props.config) {
+                if (!this.props.config.objectReactor) {
                     objectReactorType = 'IndividualObject';
-                }else{
+                } else {
                     objectReactorType = this.props.config.objectReactor[0];
                 }
             }
-            if(objectReactorType){
-                switch(objectReactorType){
+            if (objectReactorType) {
+                switch (objectReactorType) {
                     case 'IndividualObject':
                         list = this.props.spec.instances.map(function(node, index) {
-                            if(!node){
+                            if (!node) {
                                 return undefined; // stop processing this iteration
                             }
                             //check access level for details
                             readOnly = self.props.readOnly;
-                            if(node.extended){
+                            if (node.extended) {
                                 accessLevel = self.checkAccess(user, self.props.datasetURI, node.value, '');
-                                if(!accessLevel.access){
+                                if (!accessLevel.access) {
                                     readOnly = true;
                                 }
                             }
-                            return (
-                                <IndividualObject key={index} inEditMode={self.props.inEditMode} isNewValue={false} readOnly={readOnly} spec={node} datasetURI={self.props.datasetURI} resource={self.props.resource} property={self.props.spec.propertyURI} isOnlyChild={isOnlyChild}
-                                onCreate={self.props.onCreateIndividualObject.bind(self)} onDelete={self.props.onDeleteIndividualObject.bind(self)} onUpdate={self.props.onUpdateIndividualObject.bind(self)} onDetailCreate={self.props.onDetailCreateIndividualObject.bind(self)} onDetailUpdate={self.props.onDetailUpdateIndividualObject.bind(self)} onShowDetail={self.handleShowDetails.bind(self)} config={self.configMinus(self.props.config, ['objectReactor'])} objectTypes={self.props.ObjectReactor ? self.props.ObjectReactor.objectTypes : {}} objectProperties={self.props.ObjectReactor ? self.props.ObjectReactor.objectProperties : {}}/>
-                            );
+                            return (<IndividualObject key={index} inEditMode={self.props.inEditMode} isNewValue={false} readOnly={readOnly} spec={node} datasetURI={self.props.datasetURI} resource={self.props.resource} property={self.props.spec.propertyURI} isOnlyChild={isOnlyChild} onCreate={self.props.onCreateIndividualObject.bind(self)} onDelete={self.props.onDeleteIndividualObject.bind(self)} onUpdate={self.props.onUpdateIndividualObject.bind(self)} onDetailCreate={self.props.onDetailCreateIndividualObject.bind(self)} onDetailUpdate={self.props.onDetailUpdateIndividualObject.bind(self)} onShowDetail={self.handleShowDetails.bind(self)} config={self.configMinus(self.props.config, ['objectReactor'])} objectTypes={self.props.ObjectReactor
+                                ? self.props.ObjectReactor.objectTypes
+                                : {}} objectProperties={self.props.ObjectReactor
+                                ? self.props.ObjectReactor.objectProperties
+                                : {}}/>);
                         });
-                    break;
+                        break;
                     case 'AggregateObject':
                         list = <AggregateObject inEditMode={self.props.inEditMode} readOnly={self.props.readOnly} isNewValue={false} spec={self.props.spec} datasetURI={self.props.datasetURI} resource={self.props.resource} property={self.props.spec.propertyURI} onIndividualDelete={self.props.onDeleteIndividualObject.bind(self)} onIndividualUpdate={self.props.onUpdateIndividualObject.bind(self)} onDelete={self.props.onDeleteAggObject.bind(self)} onUpdate={self.props.onUpdateAggObject.bind(self)} onDetailCreate={self.props.onDetailCreateIndividualObject.bind(self)} onDetailUpdate={self.props.onDetailUpdateIndividualObject.bind(self)} controlNewInsert={self.props.onControlNewInsert.bind(self)} config={self.configMinus(self.props.config, ['objectReactor'])}/>;
-                    break;
+                        break;
                     default:
                         list = this.props.spec.instances.map(function(node, index) {
-                            if(!node){
+                            if (!node) {
                                 return undefined; // stop processing this iteration
                             }
                             //check access level for details
                             readOnly = self.props.readOnly;
-                            if(node.extended){
+                            if (node.extended) {
                                 accessLevel = self.checkAccess(user, self.props.datasetURI, self.props.resource, '');
-                                if(!accessLevel.access){
+                                if (!accessLevel.access) {
                                     readOnly = true;
                                 }
                             }
-                            return (
-                                <IndividualObject key={index} inEditMode={self.props.inEditMode} readOnly={readOnly} isNewValue={false} spec={node} datasetURI={self.props.datasetURI} resource={self.props.resource} property={self.props.spec.propertyURI} isOnlyChild={isOnlyChild}
-                                onCreate={self.props.onCreateIndividualObject.bind(self)} onDelete={self.props.onDeleteIndividualObject.bind(self)} onUpdate={self.props.onUpdateIndividualObject.bind(self)} onDetailCreate={self.props.onDetailCreateIndividualObject.bind(self)} onDetailUpdate={self.props.onDetailUpdateIndividualObject.bind(self)} onShowDetail={self.handleShowDetails.bind(self)} config={self.configMinus(self.props.config, ['objectReactor'])} objectTypes={self.props.ObjectReactor ? self.props.ObjectReactor.objectTypes : {}} objectProperties={self.props.ObjectReactor ? self.props.ObjectReactor.objectProperties : {}}/>
-                            );
+                            return (<IndividualObject key={index} inEditMode={self.props.inEditMode} readOnly={readOnly} isNewValue={false} spec={node} datasetURI={self.props.datasetURI} resource={self.props.resource} property={self.props.spec.propertyURI} isOnlyChild={isOnlyChild} onCreate={self.props.onCreateIndividualObject.bind(self)} onDelete={self.props.onDeleteIndividualObject.bind(self)} onUpdate={self.props.onUpdateIndividualObject.bind(self)} onDetailCreate={self.props.onDetailCreateIndividualObject.bind(self)} onDetailUpdate={self.props.onDetailUpdateIndividualObject.bind(self)} onShowDetail={self.handleShowDetails.bind(self)} config={self.configMinus(self.props.config, ['objectReactor'])} objectTypes={self.props.ObjectReactor
+                                ? self.props.ObjectReactor.objectTypes
+                                : {}} objectProperties={self.props.ObjectReactor
+                                ? self.props.ObjectReactor.objectProperties
+                                : {}}/>);
                         });
                 }
             }
@@ -148,9 +152,7 @@ ObjectReactor.contextTypes = {
     executeAction: React.PropTypes.func.isRequired,
     getUser: React.PropTypes.func
 };
-ObjectReactor = connectToStores(ObjectReactor, [IndividualObjectStore], function (context, props) {
-    return {
-        ObjectReactor: context.getStore(IndividualObjectStore).getState()
-    };
+ObjectReactor = connectToStores(ObjectReactor, [IndividualObjectStore], function(context, props) {
+    return {ObjectReactor: context.getStore(IndividualObjectStore).getState()};
 });
 export default ObjectReactor;
--- components/reactors/PropertyReactor.js
+++ components/reactors/PropertyReactor.js
@@ -13,133 +13,162 @@ class PropertyReactor extends React.Component {
     constructor(props) {
         super(props);
     }
-    handleDeleteIndividualObject(objectValue, valueType, dataType){
-        if(!objectValue){
+    handleDeleteIndividualObject(objectValue, valueType, dataType) {
+        if (!objectValue) {
             return null;
         }
         this.context.executeAction(deleteIndividualObject, {
-          category: (this.props.config ? (this.props.config.category ? this.props.config.category[0] : '') : ''),
-          dataset: this.props.graphName,
-          resourceURI: this.props.resource,
-          propertyPath: this.props.propertyPath,
-          propertyURI: this.props.spec.propertyURI,
-          objectValue: objectValue,
-          valueType: valueType,
-          dataType: dataType
+            category: (this.props.config
+                ? (this.props.config.category
+                    ? this.props.config.category[0]
+                    : '')
+                : ''),
+            dataset: this.props.graphName,
+            resourceURI: this.props.resource,
+            propertyPath: this.props.propertyPath,
+            propertyURI: this.props.spec.propertyURI,
+            objectValue: objectValue,
+            valueType: valueType,
+            dataType: dataType
         });
     }
-    handleDeleteAggObject(changes){
-        if(!changes.length){
+    handleDeleteAggObject(changes) {
+        if (!changes.length) {
             return null;
         }
         this.context.executeAction(deleteAggObject, {
-          category: (this.props.config ? (this.props.config.category ? this.props.config.category[0] : '') : ''),
-          dataset: this.props.graphName,
-          resourceURI: this.props.resource,
-          propertyPath: this.props.propertyPath,
-          propertyURI: this.props.spec.propertyURI,
-          changes: changes
+            category: (this.props.config
+                ? (this.props.config.category
+                    ? this.props.config.category[0]
+                    : '')
+                : ''),
+            dataset: this.props.graphName,
+            resourceURI: this.props.resource,
+            propertyPath: this.props.propertyPath,
+            propertyURI: this.props.spec.propertyURI,
+            changes: changes
         });
     }
-    handleCreateIndividualObject(objectValue, valueType, dataType){
-        if(!objectValue){
+    handleCreateIndividualObject(objectValue, valueType, dataType) {
+        if (!objectValue) {
             return null;
         }
         this.context.executeAction(createIndividualObject, {
-          category: (this.props.config ? (this.props.config.category ? this.props.config.category[0] : '') : ''),
-          dataset: this.props.graphName,
-          resourceURI: this.props.resource,
-          propertyPath: this.props.propertyPath,
-          propertyURI: this.props.spec.propertyURI,
-          objectValue: objectValue,
-          valueType: valueType,
-          dataType: dataType
+            category: (this.props.config
+                ? (this.props.config.category
+                    ? this.props.config.category[0]
+                    : '')
+                : ''),
+            dataset: this.props.graphName,
+            resourceURI: this.props.resource,
+            propertyPath: this.props.propertyPath,
+            propertyURI: this.props.spec.propertyURI,
+            objectValue: objectValue,
+            valueType: valueType,
+            dataType: dataType
         });
         this.setState({inNewValueMode: 0});
     }
-    handleUpdateIndividualObject(oldObjectValue, newObjectValue, valueType, dataType){
-        if(!newObjectValue){
+    handleUpdateIndividualObject(oldObjectValue, newObjectValue, valueType, dataType) {
+        if (!newObjectValue) {
             return null;
         }
         this.context.executeAction(updateIndividualObject, {
-          category: (this.props.config ? (this.props.config.category ? this.props.config.category[0] : '') : ''),
-          dataset: this.props.graphName,
-          resourceURI: this.props.resource,
-          propertyPath: this.props.propertyPath,
-          propertyURI: this.props.spec.propertyURI,
-          oldObjectValue: oldObjectValue,
-          newObjectValue: newObjectValue,
-          valueType: valueType,
-          dataType: dataType
+            category: (this.props.config
+                ? (this.props.config.category
+                    ? this.props.config.category[0]
+                    : '')
+                : ''),
+            dataset: this.props.graphName,
+            resourceURI: this.props.resource,
+            propertyPath: this.props.propertyPath,
+            propertyURI: this.props.spec.propertyURI,
+            oldObjectValue: oldObjectValue,
+            newObjectValue: newObjectValue,
+            valueType: valueType,
+            dataType: dataType
         });
     }
-    handleUpdateAggObject(changes){
-        if(!changes.length){
+    handleUpdateAggObject(changes) {
+        if (!changes.length) {
             return null;
         }
         this.context.executeAction(updateAggObject, {
-          category: (this.props.config ? (this.props.config.category ? this.props.config.category[0] : '') : ''),
-          dataset: this.props.graphName,
-          resourceURI: this.props.resource,
-          propertyPath: this.props.propertyPath,
-          propertyURI: this.props.spec.propertyURI,
-          changes: changes
+            category: (this.props.config
+                ? (this.props.config.category
+                    ? this.props.config.category[0]
+                    : '')
+                : ''),
+            dataset: this.props.graphName,
+            resourceURI: this.props.resource,
+            propertyPath: this.props.propertyPath,
+            propertyURI: this.props.spec.propertyURI,
+            changes: changes
         });
     }
-    handleDetailCreateIndividualObject(oldObjectValue, newObjectValue, valueType, dataType, detailData){
+    handleDetailCreateIndividualObject(oldObjectValue, newObjectValue, valueType, dataType, detailData) {
         this.context.executeAction(createIndividualObjectDetail, {
-          category: (this.props.config ? (this.props.config.category ? this.props.config.category[0] : '') : ''),
-          dataset: this.props.graphName,
-          resourceURI: this.props.resource,
-          propertyPath: this.props.propertyPath,
-          propertyURI: this.props.spec.propertyURI,
-          oldObjectValue: oldObjectValue,
-          newObjectValue: newObjectValue,
-          valueType: valueType,
-          dataType: dataType,
-          detailData: detailData
+            category: (this.props.config
+                ? (this.props.config.category
+                    ? this.props.config.category[0]
+                    : '')
+                : ''),
+            dataset: this.props.graphName,
+            resourceURI: this.props.resource,
+            propertyPath: this.props.propertyPath,
+            propertyURI: this.props.spec.propertyURI,
+            oldObjectValue: oldObjectValue,
+            newObjectValue: newObjectValue,
+            valueType: valueType,
+            dataType: dataType,
+            detailData: detailData
         });
     }
-    handleDetailUpdateIndividualObject(oldObjectValue, newObjectValue, valueType, dataType, detailData){
+    handleDetailUpdateIndividualObject(oldObjectValue, newObjectValue, valueType, dataType, detailData) {
         this.context.executeAction(updateIndividualObjectDetail, {
-          category: (this.props.config ? (this.props.config.category ? this.props.config.category[0] : '') : ''),
-          dataset: this.props.graphName,
-          resourceURI: this.props.resource,
-          propertyPath: this.props.propertyPath,
-          propertyURI: this.props.spec.propertyURI,
-          oldObjectValue: oldObjectValue,
-          newObjectValue: newObjectValue,
-          valueType: valueType,
-          dataType: dataType,
-          detailData: detailData
+            category: (this.props.config
+                ? (this.props.config.category
+                    ? this.props.config.category[0]
+                    : '')
+                : ''),
+            dataset: this.props.graphName,
+            resourceURI: this.props.resource,
+            propertyPath: this.props.propertyPath,
+            propertyURI: this.props.spec.propertyURI,
+            oldObjectValue: oldObjectValue,
+            newObjectValue: newObjectValue,
+            valueType: valueType,
+            dataType: dataType,
+            detailData: detailData
         });
     }
     //removes properties from an object
     configMinus(config, props) {
         let o = {};
-        for(let p in config) {
-            if(props.indexOf(p) === -1){
-                o [p] = config [p];
+        for (let p in config) {
+            if (props.indexOf(p) === -1) {
+                o[p] = config[p];
             }
         }
         return o;
     }
     render() {
-        let propertyReactorType, propertyReactor;
-        if(this.props.config){
-            if(!this.props.config.propertyReactor){
+        let propertyReactorType,
+            propertyReactor;
+        if (this.props.config) {
+            if (!this.props.config.propertyReactor) {
                 propertyReactorType = 'IndividualProperty';
-            }else{
+            } else {
                 propertyReactorType = this.props.config.propertyReactor[0];
             }
         }
-        if(propertyReactorType){
-            switch(propertyReactorType){
+        if (propertyReactorType) {
+            switch (propertyReactorType) {
                 case 'IndividualProperty':
-                    propertyReactor = <IndividualProperty spec={this.props.spec} enableAuthentication={this.props.enableAuthentication} readOnly={this.props.readOnly} graphName={this.props.graphName} resource={this.props.resource} property={this.props.property} propertyPath= {this.props.propertyPath} config={this.configMinus(this.props.config, ['propertyReactor'])} onCreateIndividualObject={this.handleCreateIndividualObject.bind(this)} onDeleteIndividualObject={this.handleDeleteIndividualObject.bind(this)} onUpdateIndividualObject={this.handleUpdateIndividualObject.bind(this)} onDetailCreateIndividualObject={this.handleDetailCreateIndividualObject.bind(this)} onDetailUpdateIndividualObject={this.handleDetailUpdateIndividualObject.bind(this)} onUpdateAggObject={this.handleUpdateAggObject.bind(this)} onDeleteAggObject={this.handleDeleteAggObject.bind(this)}/>;
-                break;
+                    propertyReactor = <IndividualProperty spec={this.props.spec} enableAuthentication={this.props.enableAuthentication} readOnly={this.props.readOnly} graphName={this.props.graphName} resource={this.props.resource} property={this.props.property} propertyPath={this.props.propertyPath} config={this.configMinus(this.props.config, ['propertyReactor'])} onCreateIndividualObject={this.handleCreateIndividualObject.bind(this)} onDeleteIndividualObject={this.handleDeleteIndividualObject.bind(this)} onUpdateIndividualObject={this.handleUpdateIndividualObject.bind(this)} onDetailCreateIndividualObject={this.handleDetailCreateIndividualObject.bind(this)} onDetailUpdateIndividualObject={this.handleDetailUpdateIndividualObject.bind(this)} onUpdateAggObject={this.handleUpdateAggObject.bind(this)} onDeleteAggObject={this.handleDeleteAggObject.bind(this)}/>;
+                    break;
                 default:
-                    propertyReactor = <IndividualProperty spec={this.props.spec} enableAuthentication={this.props.enableAuthentication} readOnly={this.props.readOnly} graphName={this.props.graphName} resource={this.props.resource} property={this.props.property} propertyPath= {this.props.propertyPath} config={this.configMinus(this.props.config, ['propertyReactor'])} onCreateIndividualObject={this.handleCreateIndividualObject.bind(this)} onDeleteIndividualObject={this.handleDeleteIndividualObject.bind(this)} onUpdateIndividualObject={this.handleUpdateIndividualObject.bind(this)} onDetailCreateIndividualObject={this.handleDetailCreateIndividualObject.bind(this)} onDetailUpdateIndividualObject={this.handleDetailUpdateIndividualObject.bind(this)} onUpdateAggObject={this.handleUpdateAggObject.bind(this)} onDeleteAggObject={this.handleDeleteAggObject.bind(this)}/>;
+                    propertyReactor = <IndividualProperty spec={this.props.spec} enableAuthentication={this.props.enableAuthentication} readOnly={this.props.readOnly} graphName={this.props.graphName} resource={this.props.resource} property={this.props.property} propertyPath={this.props.propertyPath} config={this.configMinus(this.props.config, ['propertyReactor'])} onCreateIndividualObject={this.handleCreateIndividualObject.bind(this)} onDeleteIndividualObject={this.handleDeleteIndividualObject.bind(this)} onUpdateIndividualObject={this.handleUpdateIndividualObject.bind(this)} onDetailCreateIndividualObject={this.handleDetailCreateIndividualObject.bind(this)} onDetailUpdateIndividualObject={this.handleDetailUpdateIndividualObject.bind(this)} onUpdateAggObject={this.handleUpdateAggObject.bind(this)} onDeleteAggObject={this.handleDeleteAggObject.bind(this)}/>;
             }
         }
         return (
