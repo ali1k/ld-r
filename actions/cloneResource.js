b63d4b2276312a86f90e14c1c5af99f673c2ab3d
ali1k <hyperir@gmail.com>
2016-11-09 16:42:03 +0100

    redirect after new resource is created

--- actions/cloneResource.js
+++ actions/cloneResource.js
@@ -1,10 +1,14 @@
+import {navigateAction} from 'fluxible-router';
 export default function cloneResource(context, payload, done) {
     context.service.create('resource.clone', payload, {}, function (err, res) {
         if (err) {
             context.dispatch('CLONE_RESOURCE_FAILURE', err);
             done();
         } else {
-            console.log(res);
+            //window.open('/dataset/' + encodeURIComponent(res.datasetURI) + '/resource/' + encodeURIComponent(res.resourceURI), '_blank');
+            context.executeAction(navigateAction, {
+                url: '/dataset/' + encodeURIComponent(res.datasetURI) + '/resource/' + encodeURIComponent(res.resourceURI)
+            });
             done();
         }
     });
