d5cb61e525376d0979d5c1ae2bd76cbfaf548cef
ali1k <hyperir@gmail.com>
2016-11-15 10:50:54 +0100

    minor

--- components/Nav.js
+++ components/Nav.js
@@ -16,6 +16,10 @@ class Nav extends React.Component {
         let user = this.context.getUser();
         // console.log(user);
         let userMenu;
+        let configMenu = <a href={'/browse/' + encodeURIComponent(configDatasetURI)} className="ui item link" title="Configuration Manager">
+            <i className="ui black settings icon"></i>
+        </a>;
+
         if(enableAuthentication){
             if(user){
                 userMenu = <div className="ui right dropdown item">
@@ -28,23 +32,28 @@ class Nav extends React.Component {
                             </div>;
             }else{
                 userMenu = <div className="ui right item"> <a className="ui mini circular teal button" href="/login">Sign-in</a> &nbsp;  <a href="/register" className="ui mini circular yellow button">Register</a> </div>;
+                configMenu = '';
             }
         }
         return (
             <nav ref="defaultNavbar" className="ui blue menu inverted navbar page grid">
-                    <NavLink routeName="home" className="brand item" href='/'>
+                    <NavLink routeName="home" className="brand item" href='http://ld-r.org'>
                         {this.props.loading ? <img src="/assets/img/loader.gif" alt="loading..." style={{height: 30, width: 30}} /> : <img style={{height: 22, width: 22}} className="ui mini image" src="/assets/img/ld-reactor.gif" alt="ld-reactor" />}
                     </NavLink>
-                    <NavLink routeName="about" className="item">About {appShortTitle} </NavLink>
-                    <NavLink routeName="datasets" className="item" href="/datasets"> Datasets</NavLink>
+                    <a className="item" href="http://ld-r.org/" data-section="home">LD-R</a>
+                    <a className="item" href="http://ld-r.org/docs/quickstart.html" data-section="quickstart">
+                      Quick Start
+                    </a>
+                    <NavLink routeName="datasets" className="item" href="/">Demo</NavLink>
                     <div className="right menu">
+                        <a href="/public/js/stats.html" className="ui item link" target="_blank" title="components stat">
+                                <i className="lab icon"></i>
+                        </a>
                         <div className="item link" onClick={this.showHelpModal}>
                                 <i className="small help circle icon"></i>
                         </div>
                         {(enableDynamicReactorConfiguration || enableDynamicServerConfiguration || enableDynamicfacetsConfiguration) ?
-                            <a href={'/browse/' + encodeURIComponent(configDatasetURI)} className="ui item link" title="Configuration Manager">
-                                <i className="ui black settings icon"></i>
-                            </a>
+                            configMenu
                         : ''}
                         <a href="http://github.com/ali1k/ld-r" className="ui item link">
                                 <i className="github circle icon"></i> Github
