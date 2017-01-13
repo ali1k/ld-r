302e732e12e8524901f7c2f973c0dc20a27dffe4
ali1k <hyperir@gmail.com>
2017-01-13 12:28:09 +0100

    fixed bug loading facets config

--- actions/loadFacets.js
+++ actions/loadFacets.js
@@ -1,5 +1,6 @@
 import {appFullTitle} from '../configs/general';
 import loadDynamicFacetsConfig from './loadDynamicFacetsConfig';
+import async from 'async';
 
 export default function loadFacets(context, payload, done) {
     //timer
@@ -7,26 +8,37 @@ export default function loadFacets(context, payload, done) {
     //start = new Date().getTime();
     //dispatch action based on the parameter
     if(payload.mode === 'init'){
-        //dynamic config
-        context.executeAction(loadDynamicFacetsConfig, payload);
-        //clear facets
-        context.dispatch('CLEAR_FACETS_SUCCESS', {});
-        //used for loading progress indicator
-        context.dispatch('LOADING_DATA', {});
-        context.service.read('facet.facetsSecondLevel', payload, {timeout: 20 * 1000}, function (err, res) {
-            //end = new Date().getTime();
-            //timeElapsed = end - start;
-            if (err) {
-                context.dispatch('LOAD_FACETS_FAILURE', err);
-            } else {
-                context.dispatch('LOAD_FACETS_RESOURCES_SUCCESS', res);
+        async.parallel([
+            (cback) => {
+                //dynamic config
+                context.executeAction(loadDynamicFacetsConfig, payload, cback);
+            },
+            (cback) => {
+                //clear facets
+                context.dispatch('CLEAR_FACETS_SUCCESS', {});
+                //used for loading progress indicator
+                context.dispatch('LOADING_DATA', {});
+                context.service.read('facet.facetsSecondLevel', payload, {timeout: 20 * 1000}, function (err, res) {
+                    //end = new Date().getTime();
+                    //timeElapsed = end - start;
+                    if (err) {
+                        context.dispatch('LOAD_FACETS_FAILURE', err);
+                    } else {
+                        context.dispatch('LOAD_FACETS_RESOURCES_SUCCESS', res);
+                    }
+                    context.dispatch('UPDATE_PAGE_TITLE', {
+                        pageTitle: (appFullTitle + ' | Faceted Browser | ' + decodeURIComponent(payload.id)) || ''
+                    });
+                    context.dispatch('LOADED_DATA', {});
+                    cback();
+                });
             }
-            context.dispatch('UPDATE_PAGE_TITLE', {
-                pageTitle: (appFullTitle + ' | Faceted Browser | ' + decodeURIComponent(payload.id)) || ''
-            });
-            context.dispatch('LOADED_DATA', {});
+        ],
+        // final callback
+        (err, results) => {
             done();
         });
+
     }else if(payload.mode === 'master'){
         //used for loading progress indicator
         context.dispatch('LOADING_DATA', {});
