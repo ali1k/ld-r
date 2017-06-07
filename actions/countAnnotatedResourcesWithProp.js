5224151213128fdb506013153e50a9c720a66a02
ali1k <hyperir@gmail.com>
2017-06-07 13:26:03 +0200

    increased timeout for annotation counter

--- actions/countAnnotatedResourcesWithProp.js
+++ actions/countAnnotatedResourcesWithProp.js
@@ -1,5 +1,5 @@
 export default function countAnnotatedResourcesWithProp(context, payload, done) {
-    context.service.read('dataset.countAnnotatedResourcesWithProp', payload, {timeout: 10 * 1000}, function (err, res) {
+    context.service.read('dataset.countAnnotatedResourcesWithProp', payload, {timeout: 20 * 1000}, function (err, res) {
         if (err) {
             context.dispatch('UPDATE_ANNOTATION_STAT_ANNOTATED__FAILURE', err);
         } else {
