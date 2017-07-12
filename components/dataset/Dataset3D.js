7d9d307a5a3308c1b80ce1bdd357733c51af248d
kadevgraaf <kadegraaf@gmail.com>
2017-07-12 14:44:04 +0200

    include react-three-renderer + anime a spinning cube

--- components/dataset/Dataset3D.js
+++ components/dataset/Dataset3D.js
@@ -2,10 +2,35 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import {connectToStores} from 'fluxible-addons-react';
 import Dataset3DStore from '../../stores/Dataset3DStore';
+import * as THREE from 'three';
+import React3 from 'react-three-renderer';
+import ReactDOM from 'react-dom';
 
 class Dataset3D extends React.Component {
     constructor(props){
         super(props);
+        // construct the position vector here, because if we use 'new' within render,
+        // React will think that things have changed when they have not.
+        this.cameraPosition = new THREE.Vector3(0, 0, 5);
+
+        this.state = {
+            cubeRotation: new THREE.Euler(),
+        };
+
+        this._onAnimate = () => {
+            // we will get this callback every frame
+
+            // pretend cubeRotation is immutable.
+            // this helps with updates and pure rendering.
+            // React will be sure that the rotation has now updated.
+            this.setState({
+                cubeRotation: new THREE.Euler(
+                    this.state.cubeRotation.x + 0.1,
+                    this.state.cubeRotation.y + 0.1,
+                    0
+                ),
+            });
+        };
     }
     componentDidMount() {
         //call actions to retrieve different characteristics of a dataset
@@ -15,6 +40,10 @@ class Dataset3D extends React.Component {
 
     }
     render() {
+        //const width = window.innerWidth; // canvas width
+        //const height = window.innerHeight; // canvas height
+        const width = 800; // canvas width
+        const height = 600; // canvas height
 
         let self = this;
         return (
@@ -22,6 +51,34 @@ class Dataset3D extends React.Component {
                 <div className="ui grid">
                     <div className="ui column">
                         Dataset3D Component
+                        <React3
+                            mainCamera="camera" // this points to the perspectiveCamera which has the name set to "camera" below
+                            width={width}
+                            height={height}
+                            onAnimate={this._onAnimate}>
+                            <scene>
+                                <perspectiveCamera
+                                    name="camera"
+                                    fov={75}
+                                    aspect={width / height}
+                                    near={0.1}
+                                    far={1000}
+                                    position={this.cameraPosition}
+                                />
+                                <mesh
+                                    rotation={this.state.cubeRotation}
+                                >
+                                    <boxGeometry
+                                        width={1}
+                                        height={1}
+                                        depth={1}
+                                    />
+                                    <meshBasicMaterial
+                                        color={0x00ff00}
+                                    />
+                                </mesh>
+                            </scene>
+                        </React3>
                     </div>
                 </div>
             </div>
