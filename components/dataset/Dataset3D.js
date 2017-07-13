467b31856c23e4c9c1632773abf0f8ff9badf1d1
kadevgraaf <kadegraaf@gmail.com>
2017-07-13 17:56:43 +0200

    Added camera controls

--- components/dataset/Dataset3D.js
+++ components/dataset/Dataset3D.js
@@ -6,6 +6,7 @@ import * as THREE from 'three';
 import React3 from 'react-three-renderer';
 import ReactDOM from 'react-dom';
 import getClassFrequency from '../../actions/getClassFrequency';
+import TrackballControls from '../trackball';
 
 class Dataset3D extends React.Component {
     constructor(props){
@@ -14,17 +15,28 @@ class Dataset3D extends React.Component {
         // React will think that things have changed when they have not.
         this.cameraPosition = new THREE.Vector3(0, 0, 5);
         this.classname1;
+        this.cameraSet = false;
 
         this.state = {
             cubeRotation: new THREE.Euler(),
+            minTimePerFrame: 0,
+            rotate: true,
+            wind: true,
+            sphere: false,
+            cameraPosition: new THREE.Vector3(0, 0, 5)
         };
 
         this._onAnimate = () => {
+            if (this.controls)
+            {
+                this.controls.update();
+            }
             // we will get this callback every frame
 
             // pretend cubeRotation is immutable.
             // this helps with updates and pure rendering.
             // React will be sure that the rotation has now updated.
+            /*
             this.setState({
                 cubeRotation: new THREE.Euler(
                     this.state.cubeRotation.x + 0.1,
@@ -32,15 +44,62 @@ class Dataset3D extends React.Component {
                     0
                 ),
             });
+            */
         };
     }
+
     componentDidMount() {
-        //call actions to retrieve different characteristics of a dataset
-        //Class Names -> their total number of instances
-        //Class Names -> max number of properties
         this.context.executeAction(getClassFrequency, {
             id: this.props.datasetURI
         });
+    }
+
+    setCamera(){
+
+        console.log('setting camera');
+        //}
+        //componentDidMount() {
+        //call actions to retrieve different characteristics of a dataset
+        //Class Names -> their total number of instances
+        //Class Names -> max number of properties
+
+
+
+        const controls = new TrackballControls(
+            this.refs.mainCamera, ReactDOM.findDOMNode(this.refs.react3)
+        );
+        controls.rotateSpeed = 1.0;
+        controls.zoomSpeed = 1.2;
+        controls.panSpeed = 0.8;
+
+        controls.noZoom = false;
+        controls.noPan = false;
+
+        controls.staticMoving = true;
+        controls.dynamicDampingFactor = 0.3;
+
+        controls.addEventListener('change', () => {
+            this.setState({
+                cameraPosition: this.refs.mainCamera.position,
+            });
+        });
+
+        this.controls = controls;
+
+    }
+    componentWillUnmount() {
+        if (this.controls)
+        {
+            this.controls.dispose();
+            delete this.controls;
+        }
+    }
+    componentDidUpdate(){
+        if (this.props.Dataset3DStore.dataset.classes.length && !this.cameraSet)
+        {
+            this.setCamera();
+            this.cameraSet = true;
+        }
     }
     render() {
         //const width = window.innerWidth; // canvas width
@@ -50,6 +109,7 @@ class Dataset3D extends React.Component {
 
         let classname1;
         let freq1;
+
 
         if (this.props.Dataset3DStore.dataset.classes.length)
         {
@@ -66,18 +126,26 @@ class Dataset3D extends React.Component {
                             Classname: {classname1}<br />
                             Frequency: {freq1}<br />
                             <React3
-                                mainCamera="camera" // this points to the perspectiveCamera which has the name set to "camera" below
+                                ref="react3"
+                                mainCamera="mainCamera" // this points to the perspectiveCamera which has the name set to "camera" below
                                 width={width}
                                 height={height}
+                                antialias
+                                gammaInput
+                                gammaOutput
+                                shadowMapEnabled
+                                shadowMapDebug
                                 onAnimate={this._onAnimate}>
                                 <scene>
                                     <perspectiveCamera
-                                        name="camera"
+                                        name="mainCamera"
+                                        ref="mainCamera"
                                         fov={75}
                                         aspect={width / height}
+                                        position={this.cameraPosition}
                                         near={0.1}
                                         far={1000}
-                                        position={this.cameraPosition}
+                                        lookAt={this.state.rotate ? this.scenePosition : null}
                                     />
                                     <mesh
                                         rotation={this.state.cubeRotation}
@@ -88,7 +156,7 @@ class Dataset3D extends React.Component {
                                             depth={1}
                                         />
                                         <meshBasicMaterial
-                                            color={0x00ff00}
+                                            color={0xDDDDDD}
                                         />
                                     </mesh>
                                 </scene>
@@ -103,6 +171,7 @@ class Dataset3D extends React.Component {
         {
             return(<div>Loading</div>);
         }
+
 
     }
 }
