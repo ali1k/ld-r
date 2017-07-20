d8efb0f75bd7c056cfd4ef66385eeeafd0781854
Ali <hyperir@gmail.com>
2017-07-20 11:21:32 +0200

    added more error handling

--- actions/prepareFacetsFromQueryAPI.js
+++ actions/prepareFacetsFromQueryAPI.js
@@ -11,58 +11,75 @@ let parseQuery = function(query){
     ////console.log('//--------------------------------//');
     let selection = {};
     let tmpPropMap ={};
-    parseQuery.where[0].patterns.forEach((el)=>{
-        if(el.type === 'query'){
-            el.where[0].patterns.forEach((el2)=>{
-                //console.log('//--------------------------------//');
-                if(el2.type === 'bgp'){
-                    el2.triples.forEach((el3)=>{
-                        //todo: handle property path
-                        //console.log(el3.predicate);
-                        if(!selection[el3.predicate]){
-                            if(el3.predicate.type && el3.predicate.type=== 'path'){
-                                selection[el3.predicate.items.join('->')] = [];
-                                tmpPropMap[el3.object]= el3.predicate.items.join('->');
-                            }else{
-                                selection[el3.predicate] = [];
-                                tmpPropMap[el3.object]= el3.predicate;
-                            }
+    if(parseQuery.where[0].patterns && parseQuery.where[0].patterns.isArray){
+        parseQuery.where[0].patterns.forEach((el)=>{
+            if(el.type === 'query'){
+                if(el.where[0].patterns && el.where[0].patterns.isArray){
+                    el.where[0].patterns.forEach((el2)=>{
+                        //console.log('//--------------------------------//');
+                        if(el2.type === 'bgp'){
+                            if (el2.triples && el2.triples.isArray){
+                                el2.triples.forEach((el3)=>{
+                                    //todo: handle property path
+                                    //console.log(el3.predicate);
+                                    if(!selection[el3.predicate]){
+                                        if(el3.predicate.type && el3.predicate.type=== 'path'){
+                                            selection[el3.predicate.items.join('->')] = [];
+                                            tmpPropMap[el3.object]= el3.predicate.items.join('->');
+                                        }else{
+                                            selection[el3.predicate] = [];
+                                            tmpPropMap[el3.object]= el3.predicate;
+                                        }
 
+                                    }
+                                    //id there is already a value
+                                    if(el3.object.indexOf('?v') === -1){
+                                        selection[el3.predicate].push({valueType:'', dataType:'', value: el3.object.replace(new RegExp('"', 'g'), '')});
+                                    }else{
+                                        tmpPropMap[el3.object]= el3.predicate;
+                                    }
+                                });
+                            }
                         }
-                        //id there is already a value
-                        if(el3.object.indexOf('?v') === -1){
-                            selection[el3.predicate].push({valueType:'', dataType:'', value: el3.object.replace(new RegExp('"', 'g'), '')});
-                        }else{
-                            tmpPropMap[el3.object]= el3.predicate;
+                        if(el2.type === 'filter'){
+                            if (el2.expression.args && el2.expression.args.isArray){
+                                el2.expression.args.forEach((el4)=>{
+                                    if(el4.operator === '&&'){
+                                        if (el4.args && el4.args.isArray){
+                                            el4.args.forEach((el5)=>{
+                                                if (el4.args[1] && el4.args[1].isArray){
+                                                    el4.args[1].forEach((el6)=>{
+                                                        selection[tmpPropMap[el5.args[0].args[0]]].push({valueType:'', dataType:'', value: el6.replace(new RegExp('"', 'g'), '')});
+                                                    });
+                                                    //console.log(el5.args);
+                                                    //console.log('------');
+                                                }
+                                            });
+                                        }
+                                    }
+                                    if(el4.operator === 'in'){
+                                        //todo: handle array
+                                        //console.log(el4.args[0].args[0]);
+                                        //console.log(el4.args[1][0]);
+                                        //console.log(el4.args[0].args[0]);
+                                        if (el4.args[1] && el4.args[1].isArray){
+                                            el4.args[1].forEach((el6)=>{
+                                                selection[tmpPropMap[el4.args[0].args[0]]].push({valueType:'', dataType:'', value: el6.replace(new RegExp('"', 'g'), '')});
+                                            });
+                                        }
+
+                                    }
+                                })
+                            }
                         }
                     });
                 }
-                if(el2.type === 'filter'){
-                    el2.expression.args.forEach((el4)=>{
-                        if(el4.operator === '&&'){
-                            el4.args.forEach((el5)=>{
-                                el4.args[1].forEach((el6)=>{
-                                    selection[tmpPropMap[el5.args[0].args[0]]].push({valueType:'', dataType:'', value: el6.replace(new RegExp('"', 'g'), '')});
-                                });
-                                //console.log(el5.args);
-                                //console.log('------');
-                            });
-                        }
-                        if(el4.operator === 'in'){
-                            //todo: handle array
-                            //console.log(el4.args[0].args[0]);
-                            //console.log(el4.args[1][0]);
-                            //console.log(el4.args[0].args[0]);
-                            el4.args[1].forEach((el6)=>{
-                                selection[tmpPropMap[el4.args[0].args[0]]].push({valueType:'', dataType:'', value: el6.replace(new RegExp('"', 'g'), '')});
-                            });
-                        }
-                    })
-                }
-            });
 
-        }
-    });
+
+            }
+        });
+    }
+
     ////console.log(selection);
     //console.log(tmpPropMap);
     //console.log(JSON.stringify(parseQuery));
