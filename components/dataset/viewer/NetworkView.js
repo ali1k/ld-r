0bc22ea119b3fde8682641d6ead6096fa8dcdd09
Ali <hyperir@gmail.com>
2017-08-10 09:55:11 +0200

    multi color networks

--- components/dataset/viewer/NetworkView.js
+++ components/dataset/viewer/NetworkView.js
@@ -1,36 +1,43 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import URIUtil from '../../utils/URIUtil';
+import chroma from 'chroma-js';
 
 class NetworkView extends React.Component {
     componentDidMount() {}
     getXYZ(propsForAnalysis){
         let c = 0, x, y, z, xLabel, yLabel, zLabel;
+        let others = {};
         for(let prop in propsForAnalysis){
             c++;
-            if(c ==1){
+            if(c == 1){
                 x = propsForAnalysis[prop];
                 xLabel = prop;
             }
-            if(c ==2){
+            if(c == 2){
                 y = propsForAnalysis[prop];
                 yLabel = prop;
             }
-            if(c ==3){
+            if(c == 3){
                 z = propsForAnalysis[prop];
                 zLabel = prop;
             }
+            if(c > 3){
+                others[prop] = propsForAnalysis[prop];
+            }
         }
-        return {x: x, y: y, z: z, xLabel: xLabel, yLabel: yLabel, zLabel: zLabel};
+        return {x: x, y: y, z: z, xLabel: xLabel, yLabel: yLabel, zLabel: zLabel, others: others};
     }
     render() {
         let self = this;
-        let Sigma, RandomizeNodePositions, RelativeSize, ForceAtlas2;
+        let colorGroup = {};
+        let Sigma, RandomizeNodePositions, RelativeSize, ForceAtlas2, SigmaEnableWebGL;
         if (process.env.BROWSER) {
             Sigma = require('react-sigma').Sigma;
             RandomizeNodePositions = require('react-sigma').RandomizeNodePositions;
             RelativeSize = require('react-sigma').RelativeSize;
             ForceAtlas2 = require('react-sigma').ForceAtlas2;
+            SigmaEnableWebGL = require('react-sigma').SigmaEnableWebGL;
         }
         let network = {nodes: [], edges: []};
         let data = [];
@@ -58,57 +65,75 @@ class NetworkView extends React.Component {
                 if(!yLabel){
                     yLabel = xyz.yLabel;
                 }
-                if(xyz.z){
-                    //3D
-                    if(!zLabel){
-                        zLabel = xyz.zLabel;
+                if(xyz.z && !zLabel){
+                    zLabel = xyz.zLabel;
+                }
+                //collect all other attributes
+                if(Object.keys(xyz.others).length){
+                    instances.push({uri: node.v, title: title , x: xyz.x, y: xyz.y, z: xyz.z, others: xyz.others});
+                    //define
+                    for(let prop in xyz.others){
+                        if(!colorGroup[prop]){
+                            colorGroup[prop] = chroma.random().hex();
+                        }
                     }
-                    instances.push({uri: node.v, title: title , x: xyz.x, y: xyz.y, z: xyz.z});
                 }else{
-                    if(xyz.y){
+                    if(zLabel){
+                        //3D
+                        instances.push({uri: node.v, title: title , x: xyz.x, y: xyz.y, z: xyz.z});
+                    }else{
                         //2D
                         instances.push({uri: node.v, title: title , x: xyz.x, y: xyz.y});
-                    }else{
-                        //1D
-                        instances.push({uri: node.v, title: title , x: xyz.x});
                     }
-
                 }
+
             });
             let nodesObj = {}, edgesObj = {};
             let nodes = [], edges = [];
             instances.forEach((node, index) => {
                 if(!nodesObj[node.uri]){
                     nodesObj[node.uri] = 1;
-                    nodes.push({id: node.uri, label: node.title, color: '#8884d8'});
+                    nodes.push({id: node.uri, label: node.title, color: '#1a75ff'});
                 }
                 if(!nodesObj[node.x]){
                     nodesObj[node.x] = 1;
-                    nodes.push({id: node.x, label: URIUtil.getURILabel(node.x), color: '#82ca9d'});
+                    nodes.push({id: node.x, label: URIUtil.getURILabel(node.x), color: '#329F5B'});
                 }
                 if(!edgesObj[node.uri+'_'+node.x]){
                     edgesObj[node.uri+'_'+node.x] = 1;
-                    edges.push({id: node.uri+'_'+node.x, source: node.uri, target: node.x, label: xLabel});
+                    edges.push({id: node.uri+'_'+node.x, source: node.uri, target: node.x, label: xLabel, color: '#329F5B'});
                 }
                 //for y and z dimensions
                 if(yLabel){
                     if(!nodesObj[node.y]){
                         nodesObj[node.y] = 1;
-                        nodes.push({id: node.y, label: URIUtil.getURILabel(node.y), color: '#ff3399'});
+                        nodes.push({id: node.y, label: URIUtil.getURILabel(node.y), color: '#D9E228'});
                     }
                     if(!edgesObj[node.uri+'_'+node.y]){
                         edgesObj[node.uri+'_'+node.y] = 1;
-                        edges.push({id: node.uri+'_'+node.y, source: node.uri, target: node.y, label: yLabel});
+                        edges.push({id: node.uri+'_'+node.y, source: node.uri, target: node.y, label: yLabel, color: '#D9E228'});
                     }
                 }
                 if(zLabel){
                     if(!nodesObj[node.z]){
                         nodesObj[node.z] = 1;
-                        nodes.push({id: node.z, label: URIUtil.getURILabel(node.z), color: '#caaf8f'});
+                        nodes.push({id: node.z, label: URIUtil.getURILabel(node.z), color: '#E9488C'});
                     }
                     if(!edgesObj[node.uri+'_'+node.z]){
                         edgesObj[node.uri+'_'+node.z] = 1;
-                        edges.push({id: node.uri+'_'+node.z, source: node.uri, target: node.z, label: zLabel});
+                        edges.push({id: node.uri+'_'+node.z, source: node.uri, target: node.z, label: zLabel, color: '#E9488C'});
+                    }
+                }
+                if(node.others){
+                    for(let prop in node.others){
+                        if(!nodesObj[node.others[prop]]){
+                            nodesObj[node.others[prop]] = 1;
+                            nodes.push({id: node.others[prop], label: URIUtil.getURILabel(node.others[prop]), color: colorGroup[prop]});
+                        }
+                        if(!edgesObj[node.uri+'_'+node.others[prop]]){
+                            edgesObj[node.uri+'_'+node.others[prop]] = 1;
+                            edges.push({id: node.uri+'_'+node.others[prop], source: node.uri, target: node.others[prop], label: prop, color: colorGroup[prop]});
+                        }
                     }
                 }
             });
