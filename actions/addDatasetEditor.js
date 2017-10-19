ca00d937316dd175829126cb8d7a5e6412a0c6df
Ali <hyperir@gmail.com>
2017-10-19 17:35:15 +0200

    delete all

--- actions/addDatasetEditor.js
+++
@@ -1,6 +0,0 @@
-import {navigateAction} from 'fluxible-router';
-export default function addDatasetEditor(context, payload, done) {
-    context.service.create('admin.datasetEditor', payload, {}, function (err, res) {
-        done();
-    });
-}
--- actions/loadDynamicFacetsConfig.js
+++
@@ -1,7 +0,0 @@
-export default function loadDynamicFacetsConfig(context, payload, done) {
-    context.service.read('facet.dynamicConfig', payload, {timeout: 20 * 1000}, function (err, res) {
-        context.dispatch('LOAD_FACETS_CONFIG', res);
-        done();
-    });
-
-}
