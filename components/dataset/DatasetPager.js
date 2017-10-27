48a17c82678427f77adfa39a83e15044ee717f93
Ali <hyperir@gmail.com>
2017-10-27 17:26:42 +0200

    fixed show all error

--- components/dataset/DatasetPager.js
+++ components/dataset/DatasetPager.js
@@ -60,6 +60,7 @@ class DatasetPager extends React.Component {
         this.context.executeAction(searchInDataset, {
             id: this.props.datasetURI,
             selection: this.props.selection,
+            pivotConstraint: this.props.pivotConstraint,
             searchTerm: term
         });
     }
