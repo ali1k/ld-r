b06f340b6a83145bfde8589f644c80fa3bd7c64e
Ali <hyperir@gmail.com>
2017-11-24 14:21:21 +0100

    do not show loader on annotations

--- actions/annotateText.js
+++ actions/annotateText.js
@@ -1,5 +1,4 @@
 export default function annotateText(context, payload, done) {
-    context.dispatch('LOADING_DATA', {});
     context.service.read('dbpedia.spotlight', payload, {timeout: 20 * 1000}, function (err, res) {
         if (err) {
             let error_res = {tags: [], id: payload.id, msg: err};
@@ -7,7 +6,6 @@ export default function annotateText(context, payload, done) {
         } else {
             context.dispatch('UPDATE_ANNOTATION_TAGS', res);
         }
-        context.dispatch('LOADED_DATA', {});
         done(null, res);
     });
 }
