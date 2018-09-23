64307b935ead97fea7060be2748c786ad1274683
ali1k <hyperir@gmail.com>
2018-09-23 20:51:33 +0200

    minor fix

--- configs/general.sample.js
+++ configs/general.sample.js
@@ -45,7 +45,7 @@ export default {
     //will enable/disable auto config
     enableAutomaticConfiguration: 0,
     //the path to the upload folder
-    uploadFolder: ['/uploaded'],
+    uploadFolder: ['./uploaded'],
     //will enable csv imports
     enableCSVImport: 1,
     //graph that stores your mapping configurations for imprting other formats such as CSV
