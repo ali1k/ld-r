43579048138b0af3ec1f20b569c11d7a341a33ed
Ali Khalili <hyperir@gmail.com>
2019-01-04 08:41:21 +0100

    Update Dataset.js

--- components/dataset/Dataset.js
+++ components/dataset/Dataset.js
@@ -88,7 +88,7 @@ class Dataset extends React.Component {
             <div className="ui list">
                 <div className="item">
                     <div  className="medium ui basic icon labeled button" onClick={this.props.onCreateResource.bind(this, this.props.datasetURI, templateResource)}>
-                        <i className="cube square large blue icon "></i> <i className="add black icon"></i> Add a New Resource
+                        <i className="cube large blue icon "></i> <i className="add black icon"></i> Add a New Resource
                     </div>
                 </div>
                 <br/>
